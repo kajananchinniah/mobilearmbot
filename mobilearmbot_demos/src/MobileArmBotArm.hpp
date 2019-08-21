869f309efdd3eaa725dd222899c347d050089bd3
Kajanan Chinniah <kajananchinniah@gmail.com>
2019-08-21 18:00:48 -0400

    Rewrote MobileArmBotArm.hpp

--- mobilearmbot_demos/src/MobileArmBotArm.hpp
+++ mobilearmbot_demos/src/MobileArmBotArm.hpp
@@ -13,8 +13,29 @@
 class MobileArmBotArm
 {
    public:
-      
+      MobileArmBotArm(ros::NodeHandle nh);
+
    private:
       ros::NodeHandle nh;
+      ros::Subscriber laser_sub;
 
+      int n_objects_to_grasp; //currently only supports 1 (i.e. the closest object)
+      int n_collision_objects; 
+
+      float obj_radius; //currently will only support the beer can found from gazebo
+      float obj_height;
+      float obj_x;
+      float obj_y;
+      float obj_z;
+
+      std::vector<moveit_msgs::Grasp> grasps;
+      std::vector<moveit_msgs::CollisionObject> collision_objects;
+      tf2::Quaternion orientation;
+
+
+      void laserScanCallback(const sensor_msgs::LaserScan& msg);
+      void openEndEffector(trajectory_msgs::JointTrajectory& posture);
+      void closeEndEffector(trajectory_msgs::JointTrajectory& posture);
+      void pick(moveit::planning_interface::MoveGroupInterface& move_group);
+      void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& planning_scene_interface);
 };
